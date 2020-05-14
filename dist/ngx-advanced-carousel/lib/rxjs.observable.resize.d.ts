ce27234085843515dc208b15c85acc1b35b8eaf3
Celal Cagkan Uguz <celalcagkanuguz@Celals-MacBook-Pro-2.local>
2020-05-14 16:24:36 +0300

    Removing dist and set main from package

--- dist/ngx-advanced-carousel/lib/rxjs.observable.resize.d.ts
+++
@@ -1,8 +0,0 @@
-import { Observable } from 'rxjs';
-/**
- * An observable creator for element resize.
- * @param elm the watch element.
- * @param cb when resize complete, call back function.
- * @param time resize emit time, default is 200
- */
-export declare function resizeObservable(elm: HTMLElement, cb: () => void, time?: number): Observable<any>;
--- dist/ngx-advanced-carousel/public-api.d.ts
+++
@@ -1,4 +0,0 @@
-export * from './lib/rxjs.observable.resize';
-export * from './lib/ngx-advanced-carousel-item.directive';
-export * from './lib/ngx-advanced-carousel.component';
-export * from './lib/ngx-advanced-carousel.module';
